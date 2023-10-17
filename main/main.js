7e0d63b1e48f529d448bfaff417332d9944ffb92
OlgaRomanchenko <olgaromanch3@gmail.com>
2023-10-17 21:19:59 +0300

    practic task2

--- main/main.js
+++ main/main.js
@@ -1,71 +1,17 @@
-function fibonachi(count){
-    let fib = [];
-    for(let i=0; i<count; i++){
-        if (i==0) fib[i]=1;
-        else if (i==1) fib[i]=2;
-        else fib[i] = fib[i-2]+fib[i-1];
-     }
-     return fib;
-}
 
-let f = fibonachi(7);
-console.log(f);
-
-function factorial(n) {
-    let fact = 1;
-    if (n==0) return fact;
-    for(let i=1; i<=n; i++){
-        fact *= i;
+function calculateVolumeAndArea(num){
+    let v_square = Math.pow(num,3);
+    let s_square = 6 * Math.pow(num,2);
+    if (typeof num === "string"){
+        return  (`При вычислении произошла ошибка`);
     }
-    return fact;
-}
-
-let a = factorial(0);
-console.log(a);
-
-let b = factorial(3);
-console.log(b);
-
-let addAndMul = function(num){
-    num += "" ;
-    let add = 0, mul = 1;
-    for(let i=0; i<= num.length; i++){
-        add +=  +num[i];
-        mul *= +num[i];      
+    if (num < 0){
+        return  (`При вычислении произошла ошибка`);
     }
-    return {
-        "Сумма ": add,
-        "Произведение": mul
-    }
-};
-
-console.log(addAndMul("456"));
-
-function revers(number){
-    number += "";
-    let reversNumber = "";
-    for(let i=number.length-1; i>=0; i--){
-        reversNumber += number[i];
-    }
-    return +reversNumber;
-}
-
-console.log(revers(4326));
-
-function randomNumber(){
-    let number = Math.floor(Math.random()*100);
-    for(let count=1; count<=10; count++){
-        let result = +prompt(`Попытка № ${count} Введите число `);
-        if(result == number){
-            return alert(`вы угадали число.Попыток ${count} Число ${number}`);
-        }else if(result < number){
-            alert (`Ваше число ${result} меньше загаданного!`);
-        }else if(result > number){
-            alert (`Ваше число ${result} больше загаданного!`);
-        }else{
-            return alert(`вы  не угадали число. Число ${number}`); 
-        }
+    else{
+        return (`Объем куба: ${v_square}, площадь всей поверхности: ${s_square}`);
     }
 }
 
-randomNumber();
+
+console.log(calculateVolumeAndArea('44'));
